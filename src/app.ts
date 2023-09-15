26f1d3ad30bf3b1116ad83314d4437f40e574ae8
Matheus Silva <matheus_lsilva@hotmail.com>
2023-09-15 18:12:35 -0300

    feat: add rate limit

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import 'express-async-errors';
 import express, { Application } from 'express';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 
 const app: Application = express();
-const a = 'po';
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: 'Too many requests, please try again later',
+});
+
 app.use(express.json());
+app.use(limiter);
 app.use(cors());
 
 export default app;
