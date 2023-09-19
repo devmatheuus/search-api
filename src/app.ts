8d0ce3da883282c90711093436300b4c39d730ac
Matheus Silva <matheus_lsilva@hotmail.com>
2023-09-19 10:51:40 -0300

    feat: remove erl

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import 'express-async-errors';
 import express, { Express } from 'express';
 import cors from 'cors';
-import rateLimit from 'express-rate-limit';
 import { appRoutes } from './routes/index.routes';
 import { errorMiddleware } from './middlewares/error.middleware';
 import swaggerJsDoc from 'swagger-jsdoc';
@@ -9,14 +8,6 @@ import { swaggerOptions } from './documentation/swagger-settings';
 import swaggerUi from 'swagger-ui-express';
 
 const app: Express = express();
-app.set('trust proxy', true);
-
-// Protection setting
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  message: 'Too many requests, please try again later',
-});
 
 // Docs settings
 const swaggerSpec = swaggerJsDoc(swaggerOptions);
@@ -24,7 +15,6 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Global Middlewares
 app.use(express.json());
-app.use(limiter);
 app.use(cors());
 appRoutes(app);
 app.use(errorMiddleware);
